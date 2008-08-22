316c8dde9966e42144b57d2673aa8f1c9c965bd1
unknown <static_fiend@circa1984.com>
2008-08-22 13:48:25 -0700

    Changed bottom text to reflect new JW FLV, also updated page.

--- play.php
+++ play.php
@@ -213,7 +213,7 @@ if ($fullscreen < 1)
 
 if (($format == "mov" || $format == "mp4") && $fullscreen < 1)
 {
-	echo "<p><small>Uses <a href=\"http://www.jeroenwijering.com/?item=JW_FLV_Media_Player\">JW FLV Media Player 4.0</a>, player skin <a href=\"http://www.longtailvideo.com/skins.asp\">Kleur</a> modified by jawbroken, PHP by Static Fiend.  Source code <a href=\"http://github.com/StaticFiend/grimfiend/tree/master\">here</a>.</small></p>\n";
+	echo "<p><small>Uses <a href=\"http://www.jeroenwijering.com/?item=JW_FLV_Media_Player\">JW FLV Media Player 4.1</a>, player skin <a href=\"http://www.longtailvideo.com/skins.asp\">Kleur</a> modified by jawbroken, PHP by Static Fiend.  Source code <a href=\"http://github.com/StaticFiend/grimfiend/tree/master\">here</a>.</small></p>\n";
 	echo "<p><a href=\"http://validator.w3.org/check?uri=referer\"><img src=\"http://www.w3.org/Icons/valid-xhtml10-blue\" alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" /></a>\n";
 	echo "<a href=\"http://jigsaw.w3.org/css-validator/check/referer\"><img style=\"border:0;width:88px;height:31px\" src=\"http://jigsaw.w3.org/css-validator/images/vcss\" alt=\"Valid CSS!\" /></a></p>\n";
 }
